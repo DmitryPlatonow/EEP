3540f35b93e4612f2cf947354ce95deef60cdb60
DmitryPlatonow <33650104+DmitryPlatonow@users.noreply.github.com>
2018-10-23 18:23:21 +0300

    Smash Code

--- EEP.API/Controllers/AccountController.cs
+++ EEP.API/Controllers/AccountController.cs
@@ -1,9 +1,18 @@
 ﻿using AutoMapper;
 using EEP.API.Models;
+using EEP.BL.Classes;
+using EEP.DAL;
 using EEP.Entities;
+using EEP.Entities.Dto;
 using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
@@ -14,8 +23,21 @@ namespace EEP.API.Controllers
     public class AccountController : ApiController
     {
 
-       // private readonly IUserService userService;
-        private readonly UserManager<User> userManager;
+      
+        private readonly IUserService _userService;
+     
+
+        public AccountController( IUserService userService)
+        {
+        
+            _userService = userService;
+        }
+
+
+        public AccountController()
+        {
+
+        }
 
         private IAuthenticationManager AuthenticationManager
         {
@@ -25,86 +47,182 @@ namespace EEP.API.Controllers
             }
         }
 
-
-        public AccountController( UserManager<User> userManager)
-        {
-          //  this.userService = userService;
-            this.userManager = userManager;
-
-            //Todo: This needs to be moved from here.
-            this.userManager.UserValidator = new UserValidator<User>(userManager)
-            {
-                AllowOnlyAlphanumericUserNames = false
-            };
-        }
-
-        // POST api/Account/Register
+               // POST: /Account/Register
         [HttpPost]
-        [OverrideAuthorization]
-        public async Task<IHttpActionResult> Register(RegisterViewModel userModel)
+        [AllowAnonymous]
+     //   [ValidateAntiForgeryToken]
+        public async Task<IHttpActionResult> Register(RegisterViewModel model)
         {
             if (ModelState.IsValid)
             {
-                try
-                {
-                    User user = new User();
-                    Mapper.Map(userModel, user);
-                    user.DateCreated = DateTime.Now;
+                var modelDto = Mapper.Map<UserDto>(model);
+                OperationResult result;
+                 result = await _userService.Create(modelDto);
+                if (result.Succeeded)
+                    return Ok(result.Message);
+                else
+                    ModelState.AddModelError(result.Property, result.Message);
 
-                    var identityResult = await userManager.CreateAsync(user, userModel.Password);
+            }
 
-                    if (identityResult.Succeeded)
-                    {
-                        userManager.AddToRole(user.Id, "Employee");
-                        return Ok();
-                    }
-                    else
-                    {
-                        foreach (var error in identityResult.Errors)
-                        {
-                            ModelState.AddModelError(error, error);
-                        }
-                        return BadRequest(ModelState);
-                    }
-                }
-                catch (Exception ex)
-                {
-                    throw ex;
-                }
-            }
-            else
-            {
-                return BadRequest(ModelState);
-            }
+           
+
+            return BadRequest(ModelState.ToString());
         }
 
-        private async Task<IHttpActionResult> SignInAsync(User user, bool isPersistent)
-        {
-            try
-            {
-                AuthenticationManager.SignOut(DefaultAuthenticationTypes.ExternalCookie);
-                var identity = await userManager.CreateIdentityAsync(user, DefaultAuthenticationTypes.ApplicationCookie);
-                AuthenticationManager.SignIn(new AuthenticationProperties() { IsPersistent = isPersistent }, identity);
-                return Ok();
-            }
-            catch (Exception ex)
-            {
-
-                throw ex;
-            }
-
-        }
-
-        //protected override void Dispose(bool disposing)
+        ////
+        //// GET: /Account/ConfirmEmail
+        //[AllowAnonymous]
+        //public async Task<ActionResult> ConfirmEmail(Guid userId, string code)
         //{
-        //    if (disposing)
+        //    if (userId == null || code == null)
         //    {
-        //        _u.Dispose();
+        //        return View("Error");
         //    }
-
-        //    base.Dispose(disposing);
+        //    var result = await _accountService.ConfirmEmail(userId, code);
+        //    return View(result.Succeeded ? "ConfirmEmail" : "Error");
         //}
 
+        ////
+        //// GET: /Account/ForgotPassword
+        //[AllowAnonymous]
+        //public ActionResult ForgotPassword()
+        //{
+        //    return View();
+        //}
 
+        ////
+        //// POST: /Account/ForgotPassword
+        //[HttpPost]
+        //[AllowAnonymous]
+        //[ValidateAntiForgeryToken]
+        //public async Task<ActionResult> ForgotPassword(ForgotPasswordViewModel model)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+
+        //        OperationResult result = await _accountService.SendCodeToRetrievePassword(model.Email);
+
+        //        if (result.Succeeded)
+        //            return RedirectToAction("Index", "Home");
+        //        else
+        //            ModelState.AddModelError(result.Property, result.Message);
+        //    }
+
+        //    return View(model);
+        //}
+
+        ////
+        //// GET: /Account/ForgotPasswordConfirmation
+        //[AllowAnonymous]
+        //public ActionResult ForgotPasswordConfirmation()
+        //{
+        //    return View();
+        //}
+
+        ////
+        //// GET: /Account/ResetPassword
+        //[AllowAnonymous]
+        //public ActionResult ResetPassword(string code)
+        //{
+        //    return code == null ? View("Error") : View();
+        //}
+
+        ////
+        //// POST: /Account/ResetPassword
+        //[HttpPost]
+        //[AllowAnonymous]
+        //[ValidateAntiForgeryToken]
+        //public async Task<ActionResult> ResetPassword(ResetPasswordViewModel model)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        OperationResult result = await _accountService.ResetPassword(model.Email, model.Code, model.Password);
+
+        //        if (result.Succeeded)
+        //            return RedirectToAction("ResetPasswordConfirmation", "Account");
+        //        else
+        //            ModelState.AddModelError(result.Property, result.Message);
+        //    }
+
+        //    return View(model);
+        //}
+
+        ////
+        //// GET: /Account/ResetPasswordConfirmation
+        //[AllowAnonymous]
+        //public ActionResult ResetPasswordConfirmation()
+        //{
+        //    return View();
+        //}
+
+        ////
+        //// POST: /Account/LogOff
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult LogOff()
+        //{
+        //    AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
+        //    return RedirectToAction("Index", "Home", new { area = "" });
+        //}
+
+        ////
+        //// GET: /Account/ExternalLoginFailure
+        //[AllowAnonymous]
+        //public ActionResult ExternalLoginFailure()
+        //{
+        //    return View();
+        //}
+
+        //#region Helpers
+        //// Used for XSRF protection when adding external logins
+        //private const string XsrfKey = "XsrfId";
+
+        //private void AddErrors(IdentityResult result)
+        //{
+        //    foreach (var error in result.Errors)
+        //    {
+        //        ModelState.AddModelError("", error);
+        //    }
+        //}
+
+        //private ActionResult RedirectToLocal(string returnUrl)
+        //{
+        //    if (Url.IsLocalUrl(returnUrl))
+        //    {
+        //        return Redirect(returnUrl);
+        //    }
+        //    return RedirectToAction("Index", "Home", new { area = "" });
+        //}
+
+        //internal class ChallengeResult : HttpUnauthorizedResult
+        //{
+        //    public ChallengeResult(string provider, string redirectUri)
+        //        : this(provider, redirectUri, null)
+        //    {
+        //    }
+
+        //    public ChallengeResult(string provider, string redirectUri, string userId)
+        //    {
+        //        LoginProvider = provider;
+        //        RedirectUri = redirectUri;
+        //        UserId = userId;
+        //    }
+
+        //    public string LoginProvider { get; set; }
+        //    public string RedirectUri { get; set; }
+        //    public string UserId { get; set; }
+
+        //    public override void ExecuteResult(ControllerContext context)
+        //    {
+        //        var properties = new AuthenticationProperties { RedirectUri = RedirectUri };
+        //        if (UserId != null)
+        //        {
+        //            properties.Dictionary[XsrfKey] = UserId;
+        //        }
+        //        context.HttpContext.GetOwinContext().Authentication.Challenge(properties, LoginProvider);
+        //    }
+        //}
+        //#endregion
     }
 }
